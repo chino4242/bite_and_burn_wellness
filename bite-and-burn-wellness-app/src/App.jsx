3a57bf2e62398e9f2c19637d09895d3286a74c5f
chino4242 <ryanj.contino@gmail.com>
2025-07-04 06:09:30 -0400

    Passes data into output and displays on the page

--- bite-and-burn-wellness-app/src/App.jsx
+++ bite-and-burn-wellness-app/src/App.jsx
@@ -21,8 +21,8 @@ function App() {
       <div>
         <Header />
         <Navbar />
-        <Capture />
-        <Output />
+        <Capture onDataSubmit={handleDataCapture} />
+        <Output data ={submittedData} />
       </div>
     </>
   )
--- bite-and-burn-wellness-app/src/components/Output.jsx
+++ bite-and-burn-wellness-app/src/components/Output.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
-function Output() {
+function Output({data}) {
+    if(!data){
+        return null;
+    }
+    const capitalizedProfile = data.profile.charAt(0).toUpperCase() + data.profile.slice(1);
+    const totalCalories = (data.fat * 9) + (data.carbs * 4) + (data.protein * 4)
     return (
         <div class="col-md-6">
                 <div class="output-container bg-white p-4 rounded shadow-sm">
-                    <h1>Daily Data Output</h1>
-                    <p id="output"></p>
-                    <p id="sleep_output"></p>
-                    <p id="carbs_output"></p>
-                    <p id="protein_output"></p>
-                    <p id="fat_output"></p>
-                    <p id="calories_output"></p>
-                    <p id="water_output"></p>
-                    <p id="steps_output"></p>
-                    <p id="workout_output"></p>
-                    <p id="mood_output"></p>
+                    <h2>Daily Data Output</h2>
+                    <ul>
+                        <li>Profile: {capitalizedProfile}</li>
+                        <li>Hours Slept: {data.hoursSlept}</li>
+                        <li>Carbohydrates: {data.carbs}</li>
+                        <li>Protein: {data.protein}</li>
+                        <li>Total Calories: {totalCalories}</li>
+                        <li>Fat: {data.fat}</li>
+                        <li>Water (oz): {data.water}</li>
+                        <li>Steps: {data.steps}</li>
+                        <li>Workout (minutes): {data.workout}</li>
+                        <li>Mood/Mindset Rating (1-10): {data.mood}</li>
+                    </ul>
                 </div>
             </div>
     )
